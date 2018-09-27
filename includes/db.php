21c069dc54be20016d2b9d848a8ab53613f59c46
Dhiren Chotwani <dhirenchotwani@gmail.com>
2018-09-27 20:38:55 +0530

    Automatic Q/P v1.1

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php 
+    define("SERVER", 'localhost');
+    define("USER", 'Dhiren Chotwani');
+    define("PASSWORD", 'Activa9015');
+    define("DB", 'questionpaper');
+
+    $connection = mysqli_connect(SERVER, USER, PASSWORD, DB);
+//    if($connection){
+//        echo "We are Connected! <br>";
+//    }
+?>
